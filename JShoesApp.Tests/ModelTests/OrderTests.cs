12e5f8d1e1206f4a72e82cef4fd25df3b76dfd79
Emzzy241 <emzzyoluwole@gmail.com>
2024-11-15 11:21:33 +0100

    update OrderTests

--- JShoesApp.Tests/ModelTests/OrderTests.cs
+++ JShoesApp.Tests/ModelTests/OrderTests.cs
@@ -14,16 +14,16 @@ public class OrderTests
         Shoe shoe = new Shoe("Louis Vuitton 975", Brand.LouisVuitton, Color.Black,  100.00M, PriceRange.HundredDollarsOrLess, 10);
         Shoe shoe2 = new Shoe("Air max 150", Brand.Nike, Color.Red,  49.99M, PriceRange.FiftyDollarsOrLess, 40);
         Shoe shoe3 = new Shoe("Balenciaga Y9", Brand.Balenciaga, Color.White,  135.00M, PriceRange.MoreThanHundredDollars, 60);
-        decimal expectedTotal = 284.99m;
+        Shoe shoe4 = new Shoe("Balenciaga Y9", Brand.Balenciaga, Color.White,  125.00M, PriceRange.MoreThanHundredDollars, 60);
+        decimal expectedTotal = 409.99m;
 
         // Act
-        // List<Shoe> shoeList = new List<Shoe>(){shoe, shoe2, shoe3};
-
         Order.ShoeList.Add(shoe);
         Order.ShoeList.Add(shoe2);
         Order.ShoeList.Add(shoe3);
+        Order.ShoeList.Add(shoe4);
 
-        decimal actualTotal = Order.TotalPrice(shoeList);
+        decimal actualTotal = Order.TotalPrice(Order.ShoeList);
 
         // Assert
         Assert.Equal(expectedTotal, actualTotal);
