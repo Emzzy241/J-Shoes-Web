ae1959c154556ef1c33d6daddf3c62a69add4a68
Emzzy241 <emzzyoluwole@gmail.com>
2024-11-21 09:04:36 +0100

    fixed negative shoe prices bug

--- JShoesApp/Models/Shoe.cs
+++ JShoesApp/Models/Shoe.cs
@@ -17,7 +17,7 @@ public class Shoe
     // Using a property setter that enforces 2 decimal places
     public decimal Price { 
         get => _price; 
-        set => _price = Math.Round(value, 2);
+        set => _price = Math.Round(value < 0 ? 0 : value, 2);
     }
 
     public decimal Discount { get; set; }  
