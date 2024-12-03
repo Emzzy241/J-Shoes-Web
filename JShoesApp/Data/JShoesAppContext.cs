61454635cbb75c63f8656c85708dacddcdd59922
Emzzy241 <emzzyoluwole@gmail.com>
2024-12-03 06:29:26 +0100

    installing required packages for creating database

--- JShoesApp/Data/JShoesAppContext.cs
+++ JShoesApp/Data/JShoesAppContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using JShoesApp.Models;
 
 namespace JShoesApp.Data;
 public class JShoesAppContext : IdentityDbContext<ApplicationUser>
@@ -7,7 +8,7 @@ public class JShoesAppContext : IdentityDbContext<ApplicationUser>
 
     public DbSet<Shoe> Shoes { get; set; }
     public DbSet<Order> Orders { get; set; }
-    public DbSet<Address> Address { get; set; }
+    public DbSet<Address> Addresses { get; set; }
     
     public JShoesAppContext(DbContextOptions options): base(options){ }
 
